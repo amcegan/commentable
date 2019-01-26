f0319b110d41c293f15dca1e2deb5bc42329048a
amcegan <anthonymacegan@gmail.com>
2019-01-26 10:49:12 +0000

    Bug

--- src/main/groovy/com/centaur/plugins/Commentable.groovy
+++ src/main/groovy/com/centaur/plugins/Commentable.groovy
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.grails.comments
+package com.centaur.plugins
 
 /**
  * Marker interface used to specify a domain that can be commented on
